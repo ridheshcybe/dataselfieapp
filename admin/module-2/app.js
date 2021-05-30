03225790d437122797ee7a90444bb9141b9af1f2
cybemachine <80441770+cybemachine@users.noreply.github.com>
2021-05-30 12:00:19 +0545

    added other

--- admin/module-2/app.js
+++ admin/module-2/app.js
@@ -1,7 +1,11 @@
 var express = require('express'),
     app = express();
 
+app.use(express.static('../../assets/module-2'))
 
+app.get('/', (req, res) => {
+    res.sendFile('index.html')
+})
 
 app.listen(8000, () => {
     console.log('listing');
